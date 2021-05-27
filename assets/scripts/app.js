2d049a67fde67c3cd8c630902a63617f0fa518f0
bambie669 <raduroman94@gmail.com>
2021-05-27 22:44:15 +0300

    add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,90 +1,13 @@
-// alert('gg')
-const defaultResult = 0;
 let currentResult = defaultResult;
-let logEntries = [];
+const defaultResult = 0;
 
-// Gets input from input field
-function getUserNumberInput() {
-  return parseInt(usrInput.value);
+function add(num1, num2) {
+  const result = num1 + num2
+  return result;
 }
 
-// Generates and writes calculation log
-function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-  outputResult(currentResult, calcDescription); // from vendor file
-}
+currentResult = add(1, 2);
 
-function writeToLog(
-  operationIdentifier,
-  prevResult,
-  operationNumber,
-  newResult
-) {
-  const logEntry = {
-    operation: operationIdentifier,
-    prevResult: prevResult,
-    number: operationNumber,
-    result: newResult,
-  };
-  logEntries.push(logEntry);
-  console.log(logEntries);
-}
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
-function calculateREsult(calculationType) {
-  if (
-    calculationType !== 'ADD' &&
-    calculationType !== 'SUBSTRACT' &&
-    calculationType !== 'MULTIPLY' &&
-    calculationType !== 'DIVIDE'
-  ) {
-    return;
-  }
-
-  // if (
-  //   calculationType === 'ADD' ||
-  //   calculationType === 'SUBSTRACT' ||
-  //   calculationType === 'MULTIPLY'  ||
-  //   calculationType === 'DEVIDE' ||
-  // ) {
-    const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult;
-    let mathOperator;
-    if (calculationType === 'ADD') {
-      currentResult += enteredNumber;
-      mathOperator = '+';
-    } else if (calculationType === 'SUBSTRACT') {
-      currentResult -= enteredNumber;
-      mathOperator = '-';
-    } else if (calculationType === 'MULTIPLY') {
-      currentResult *= enteredNumber;
-      mathOperator = '*';
-    } else if (calculationType === 'DIVIDE') {
-      currentResult /= enteredNumber;
-      mathOperator = '/';
-    }
-  
-  createAndWriteOutput(mathOperator, initialResult, enteredNumber);
-  writeToLog(calculationType, initialResult, enteredNumber, currentResult);
-}
-  
-
-function add() {
-  calculateREsult('ADD');
-}
-
-function subtract() {
-  calculateREsult('SUBSTRACT');
-}
-
-function multiply() {
-  calculateREsult('MULTIPLY');
-}
-
-function divide() {
-  calculateREsult('DIVIDE');
-}
-
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
+outputResult(currentResult, 'calculationDescription');
