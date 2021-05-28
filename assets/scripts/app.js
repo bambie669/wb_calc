31349f0aa895d4ef832378c6f1f54432bd23ec56
bambie669 <raduroman94@gmail.com>
2021-05-28 21:29:58 +0300

    substract() added add() refact

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,17 +2,30 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function getUserNumberInput() {
-  return parseInt(userInput.value)
+  return parseInt(userInput.value);
 }
 
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
+  const description = `${currentResult} ${operator} ${enteredNumber}`;
+  outputResult(currentResult, description);
+
+}
 
 function add() {
-  const enteredNumber = getUserNumberInput()
-  const description = `${currentResult} + ${enteredNumber}`;
-  currentResult = currentResult + enteredNumber
-  outputResult(currentResult, description)
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput('+', initialResult, enteredNumber)
 }
 
+function substract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput('-', initialResult, enteredNumber)
+}
 
+function multiply() {}
+function divide() {}
 
 addBtn.addEventListener('click', add);
